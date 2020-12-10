4cf7bc32900ddafdb9044bc1c4bd0222ca766fab
Somuns <somuns.jx@gmail.com>
2020-12-10 18:25:38 +0800

    Section24 Class OK

---
+++ TimothyLiu/Section24/Section24/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------");
+            Test.Class.Show();
+
+
+
+
+
+            //Console.Read();
+        }
+    }
+}
---
+++ TimothyLiu/Section24/Section24/Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section24
+{
+    class Test
+    {
+        public class Class
+        {
+            public static void Show()
+            {
+                var stu1 = new Student("stu1", 1);
+                var stu2 = new Student("stu2", 2);
+                var stu3 = new Student("stu3", 3);
+                var stu4 = new Student("stu4", 4);
+                var stu5 = new Student("stu5", 5);
+            }
+
+            class Student
+            {
+                public int ID { get; set; }
+                public string Name { get; set; }
+                public static int Amount = 0;
+                public Student(string name, int id)
+                {
+                    Amount++;
+                    Name = name;
+                    ID = id;
+                    Console.WriteLine(" Student " + ID + " " + Name + " adds " + " Amount = " + Amount);
+                }
+                ~Student()  // 析构器
+                {
+                    Amount--;
+                    Console.WriteLine(" Student ID " + ID + " " + Name + " release its source " + " Amount = " + Amount);
+                }
+            }
+        }
+    }
+}
